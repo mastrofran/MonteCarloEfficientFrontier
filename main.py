4a8d08d72ecd48b0525dace99ff5b868758ad54e
Connor Monks <monkslc@gmail.com>
2018-11-03 08:54:06 -0400

    Commiting main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import portfolios
 # How to get adjusted closing prices
 adjClosingPrices = stocks.getAdjClosingPrices()
 
-# If you want to see it as a percent return from start date you can wrap it in:
+# If you want to see it as a percent return from start date you can call the method:
 percentReturn = stocks.getPercentReturnFromStartDate(adjClosingPrices)
 
 
